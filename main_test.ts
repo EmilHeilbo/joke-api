47545e1a71e262a2a2c2b837e76fa8c3ab219dd0
Emil Heilbo <19676610+EmilHeilbo@users.noreply.github.com>
2024-10-29 10:38:43 +0100

    test: check if main returns three jokes

--- main_test.ts
+++ main_test.ts
@@ -7,3 +7,12 @@ Deno.test(async function handlerTest() {
   const body = await response.text();
   assertEquals(body.length > 0, true);
 });
+
+Deno.test(async function handlerTestTheSequel() {
+  const response = await handler({} as Request, {} as Deno.ServeHandlerInfo);
+  assertEquals(response.status, 200);
+  const body = await response.text();
+  assertEquals(body.length > 0, true);
+  const jokes = body.split('\n')
+  assertEquals(jokes.length, 3);
+});
